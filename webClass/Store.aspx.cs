bd832f0e519b7b8856e4d83d5a9dc5da8d9f6c9c
yus091 <ysheng910316@gmail.com>
2023-12-01 17:11:25 +0800

    class8

--- webClass/Store.aspx.cs
+++ webClass/Store.aspx.cs
@@ -87,6 +87,8 @@ namespace webClass
             iceList.Visible = false;
             addItemBT.Visible = false;
             orderItemGridView.Visible = false;
+            totalLB.Visible = false;
+
         }
 
         protected void addItemBT_Click(object sender, EventArgs e)
@@ -95,6 +97,64 @@ namespace webClass
             if (!orderItemGridView.Visible)
             {
                 orderItemGridView.Visible = true;
+            }
+            totalLB.Visible = true;
+        }
+
+        protected void orderItemGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            countTotalLB();
+        }
+
+        private void countTotalLB()
+        {
+            string warmMsg = "";
+            int total = 0;
+            for(int i = 0; i < orderItemGridView.Rows.Count; i++)
+            {
+                if(orderItemGridView.Rows[i].Cells[4].FindControl("subTotalLB") != null)
+                {
+                    total += Convert.ToInt32(((Label)orderItemGridView.Rows[i].Cells[4].FindControl("subTotalLB")).Text);
+                }
+                cupEditCheck(ref warmMsg, i);
+            }
+            totalLB.Text = warmMsg + "總價：" + total + " 元";
+        }
+
+        private void cupEditCheck(ref string msg, int i)
+        {
+            if (orderItemGridView.Rows[i].Cells[1].FindControl("itemCupLB") != null)
+            {
+                using(Label tempCupLB = (Label)orderItemGridView.Rows[i].Cells[1].FindControl("itemCupLB"))
+                {
+                    if(tempCupLB.Text == "0")
+                    {
+                        msg = "（錯誤的杯數）";
+                        tempCupLB.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        tempCupLB.ForeColor = System.Drawing.Color.FromArgb(99, 33, 66);
+                    }
+                }
+            }
+        }
+
+        protected void orderItemGridView_RowDeleted(object sender, GridViewDeletedEventArgs e)
+        {
+            if(orderItemGridView.Rows.Count == 1)
+            {
+                totalLB.Visible = false;
+            }
+        }
+
+        protected void editCupTB_TextChanged(object sender, EventArgs e)
+        {
+            foreach(char ch in ((TextBox)sender).Text)
+            {
+                if (!Char.IsDigit(ch))
+                {
+                    ((TextBox)sender).Text = "0";                }
             }
         }
     }
--- webClass/Store.aspx.designer.cs
+++ webClass/Store.aspx.designer.cs
@@ -141,6 +141,15 @@ namespace webClass
         protected global::System.Web.UI.WebControls.GridView orderItemGridView;
 
         /// <summary>
+        /// totalLB 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label totalLB;
+
+        /// <summary>
         /// drinkData 控制項。
         /// </summary>
         /// <remarks>
